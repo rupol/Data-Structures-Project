000eed73b21f53f50030ef2e30f7234059007ac6
Ruth Poliakon <r.poliakon@gmail.com>
2020-06-18 21:03:27 -0600

    remove from head and tail methods implemented

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -80,9 +80,8 @@ class DoublyLinkedList:
             self.head = new_node
 
     # Removes the List's current head node, making the current head's next node the new head of the List. Returns the value of the removed Node.
-
     def remove_from_head(self):
-        pass
+        return self.delete(self.head)
 
     # Wraps the given value in a ListNode and inserts it as the new tail of the list. Don't forget to handle the old tail node's next pointer accordingly.
     def add_to_tail(self, value):
@@ -102,7 +101,7 @@ class DoublyLinkedList:
 
     # Removes the List's current tail node, making the current tail's previous node the new tail of the List. Returns the value of the removed Node.
     def remove_from_tail(self):
-        pass
+        return self.delete(self.tail)
 
     # Removes the input node from its current spot in the List and inserts it as the new head node of the List.
     def move_to_front(self, node):
