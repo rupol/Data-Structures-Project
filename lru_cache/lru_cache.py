0dbdccb0b49e1ed0d36182fe309c844f7982bc00
Ruth Poliakon <r.poliakon@gmail.com>
2020-06-25 21:08:37 -0600

    lru cache completed - all tests passing

--- lru_cache/lru_cache.py
+++ lru_cache/lru_cache.py
@@ -1,3 +1,6 @@
+from doubly_linked_list import DoublyLinkedList
+
+
 class LRUCache:
     """
     Our LRUCache class keeps track of the max number of nodes it
@@ -6,8 +9,13 @@ class LRUCache:
     order, as well as a storage dict that provides fast access
     to every node stored in the cache.
     """
-    def __init__(self, limit=10):
-        pass
+
+    def __init__(self, limit=3):
+        self.limit = limit
+        # holds key, value pairs
+        self.storage = {}
+        # holds keys as nodes in correct order
+        self.ordered = DoublyLinkedList()
 
     """
     Retrieves the value associated with the given key. Also
@@ -16,8 +24,16 @@ class LRUCache:
     Returns the value associated with the key or None if the
     key-value pair doesn't exist in the cache.
     """
+
     def get(self, key):
-        pass
+        # return value in storage dict
+        value = self.storage.get(key)
+        # find value in ordered list
+        node = self.ordered.contains(value)
+        if node is not None:
+            # move node to back of list
+            self.ordered.move_to_end(node)
+        return value
 
     """
     Adds the given key-value pair to the cache. The newly-
@@ -29,5 +45,32 @@ class LRUCache:
     want to overwrite the old value associated with the key with
     the newly-specified value.
     """
+
     def set(self, key, value):
-        pass
+        # add key value pair to storage
+        # if key exists, overwrite value
+        new_pair = {key: value}
+        self.storage.update(new_pair)
+
+        # add value to back of ordered list
+        self.ordered.add_to_tail(value)
+        # if length of ordered list and storage is > limit
+        if len(self.ordered) > self.limit and len(self.storage) > self.limit:
+            # remove front of ordered list
+            value = self.ordered.remove_from_head()
+            # remove from dictionary
+            self.storage = {k: v for k,
+                            v in self.storage.items() if v != value}
+
+
+cache = LRUCache(3)
+cache.set('item1', 'a')
+cache.set('item2', 'b')
+cache.set('item3', 'c')
+# print(len(cache.ordered))
+print(len(cache.storage))
+print(cache.storage)
+cache.set('item2', 'z')
+print(cache.storage)
+# print(cache.get('item1'))
+# print(len(cache.ordered))
